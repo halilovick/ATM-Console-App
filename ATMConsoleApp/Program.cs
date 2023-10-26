4cb8e1780572c44a906ecc248adb53dc3712cf5b
halilovick <khalilovic02@gmail.com>
2023-10-26 19:34:18 +0200

    Add project files.

---
+++ ATMConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ATMConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
